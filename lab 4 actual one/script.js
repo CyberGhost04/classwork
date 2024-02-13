4d6ca2364c70113fd92e18da98143f43562ef1f0
CyberGhost04 <sidhantbahuguna@gmail.com>
2024-02-13 09:35:09 +0530

    more stuff

---
+++ lab 4 actual one/script.js
@@ -0,0 +1,5 @@
+function pressed() {
+    var name = document.getElementById('name').value;
+    var regis = document.getElementById('regis').value;
+    alert('Name: ' + name + '\n' + 'Registration: ' + regis);
+}
